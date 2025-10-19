ea091f0cbe24877646581c97b00d4a8019d867e3
Abbyeeesss <espinosaabigail76@gmail.com>
2025-10-20 00:38:06 +0100

    Animacion del personaje Idle,Walk

--- ProyectoFinal_Videojuego/Assets/Scripts/Player.cs
+++ ProyectoFinal_Videojuego/Assets/Scripts/Player.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 3f;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveInput;
+
     void Start()
     {
-        Debug.Log("Player script has started.");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (moveX < 0)
+            spriteRenderer.flipX = true;
+        else if (moveX > 0)
+            spriteRenderer.flipX = false;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
